89fd2ba050d61cdff775aa93d8a7ebb32d4d0d31
senyich <arseniyneshetin@gmail.com>
2025-03-05 20:21:11 +0700

    add di dz

---
+++ Homeworks/DepensyInjectionHW 13.03/3-4/DepencyContainer/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using DepencyContainer.Services;
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public int Year { get; set; }
+    public string ElseInfo { get; set; }
+
+    public override string ToString()
+    {
+        return $"Заголовок: {Title}\n" +
+            $"Автор: {Author}\n" +
+            $"Описание: {Description}\n" +
+            $"Год: {Year}\n" +
+            $"Прочая информация: {ElseInfo}\n";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "books.txt";
+        var serviceProvider = new ServiceCollection().AddSingleton<IBookService, BookService>()
+            .BuildServiceProvider();
+        var bookService = serviceProvider.GetService<IBookService>();
+
+        bookService.AddBook(new Book { Title = "книга1", Author = "автор1", Description = "описание1", Year = 2020, ElseInfo = "жанр1" });
+        bookService.AddBook(new Book { Title = "книга2", Author = "автор2", Description = "описание2", Year = 2007, ElseInfo = "факт2" });
+
+        Console.WriteLine("Все книги:");
+        bookService.PrintBooks();
+
+        bookService.SaveToFile(filePath);
+        Console.WriteLine("Книги сохранены в файл");
+
+        bookService.LoadFromFile(filePath, 1);
+        Console.WriteLine("Книги загружены с файла");
+        bookService.PrintBooks();
+    }
+}
---
+++ Homeworks/DepensyInjectionHW 13.03/3-4/DepencyContainer/Services/BookService.cs
@@ -0,0 +1,76 @@
+﻿
+namespace DepencyContainer.Services
+{
+    public interface IBookService
+    {
+        void AddBook(Book book);
+        void PrintBooks();
+        void SaveToFile(string filePath);
+        void LoadFromFile(string filePath, int emptyLineSeparator);
+    }
+    public class BookService : IBookService
+    {
+        private List<Book> books;
+        public BookService() { books = new List<Book>(); }
+        public void AddBook(Book book) => books.Add(book);
+        public void PrintBooks()
+        {
+            foreach (var book in books)
+                Console.WriteLine(book);
+        }
+        public void SaveToFile(string filePath)
+        {
+            using FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            using StreamWriter sw = new(fs);
+            foreach (var book in books)
+            {
+                sw.WriteLine(book.Title);
+                sw.WriteLine(book.Author);
+                sw.WriteLine(book.Description);
+                sw.WriteLine(book.Year);
+                sw.WriteLine(book.ElseInfo);
+                sw.WriteLine();
+            }
+        }
+        public void LoadFromFile(string filePath, int lineSeparator)//решение не сильно красивое, но для примера пойдет
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("файл не найден");
+                return;
+            }
+            books.Clear();
+            var lines = File.ReadAllLines(filePath);
+            List<string> bookData = new();
+            int lineCounter = 0;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    lineCounter++;
+                    if (lineCounter == lineSeparator)
+                    {
+                        if (bookData.Count >= 5)
+                        {
+                            books.Add(new Book
+                            {
+                                Title = bookData[0],
+                                Author = bookData[1],
+                                Description = bookData[2],
+                                Year = int.Parse(bookData[3]),
+                                ElseInfo = bookData[4]
+                            });
+                        }
+                        bookData.Clear();
+                        lineCounter = 0;
+                    }
+                }
+                else
+                {
+                    lineCounter = 0;
+                    bookData.Add(line);
+                }
+            }
+        }
+    }
+}
